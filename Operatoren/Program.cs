7d4464268bea64c7d09ad54a1c2fbf178c0977ad
dmytro.d2 <dmytro.d2@P-0G-0B111-0427>
2022-09-02 12:13:34 +0200

    Projektdateien hinzufügen.

---
+++ Operatoren/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Aufgabe 1
+//string a = "Hallo";
+//string b = "H";
+//b += "allo";
+
+//Console.WriteLine(b);   //Ausführung: Es wurde "Hallo" ausgeführt.
+
+//Console.WriteLine(b.Length); //Ausführung: Die Buchstaben wurden gezählt.
+
+//Console.WriteLine(a == b); //Ausführung: Es zeigt, dass strings "a" und "b" die selbe größe haben.
+
+//Console.WriteLine((object)a == b); //Ausführung: Da zu "a" ein object hinzüfgt wurde, wurde das "a" größer als "b". 
+
+//Aufgabe 2
+//double x = 2.0;
+//double y = x * x + 1 + x / 4;   //Erwartetes Ergebnis: 5.5  
+//Console.Writeline(y);           //Nach der Überprüfung: 5.5
+
+//double x = 2.0;
+//double y = x * (x + 1 + x / 4);   //Erwartetes Ergebnis: 7
+//Console.WriteLine(y);             //Nach der Überprüfung: 7
+
+//bool b = (true && false) || 5 > 6; //Erwartetes Ergebnis: Fünf ist kleiner als Sechs (False)
+//Console.WriteLine(b);              //Nach der Überprüfung:(False)
+
+//int z = 50; bool numeric = z > 47 && z < 58; //Erwartetes Ergebnis: True   
+//Console.WriteLine(numeric);                  //Nach der Überprüfung: True
